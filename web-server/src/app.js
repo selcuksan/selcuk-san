70d6a0b48d0fcfcfe687c947623155c88a0ef7ec
selcuksan <18360859049@ogrenci.btu.edu.tr>
2022-04-22 09:40:13 +0300

    second commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,11 +5,30 @@ var app = express()
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("home page");
+    res.send("Homepage");
 })
 
 app.get("/test_text", (req, res) => {
     res.send("18360859049");
+})
+
+app.get("/test_html", (req, res) => {
+    res.send("<h1>Selçuk Şan</h1>");
+})
+
+app.get("/test_json", (req, res) => {
+    res.send({
+        ogrenci_no: "18360859049",
+        ad: "Selçuk",
+        soyad: "Şan"
+
+    });
+})
+app.get("/test_geocode", (req, res) => {
+    res.send("test_geocode");
+})
+app.get("/test_weather", (req, res) => {
+    res.send("test_geocode");
 })
 
 
